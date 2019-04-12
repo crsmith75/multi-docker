bf46696304012ad6cb5dab8ef72cfc9a24589718
Connor Smith <connorsmith@Connors-MacBook-Pro-2.local>
2019-04-11 21:26:36 -0400

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+//Keys for this application for all pieces to work together
+
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+};
